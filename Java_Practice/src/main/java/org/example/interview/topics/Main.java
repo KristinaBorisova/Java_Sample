2fd8ceb3377fb3d53b01450b51bf7f205f11592e
Kristina Borisova <kkristinaborisova@gmail.com>
2024-10-31 17:16:54 +0200

    Sample Boat Task

---
+++ Java_Practice/src/main/java/org/example/interview/topics/Main.java
@@ -0,0 +1,60 @@
+package org.example.interview.topics;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    //private function for calculations
+    private static int calculateBoatSize(int maxBoatCourses, int totalSheepWeight, ArrayList <Integer> sheepWeightList) {
+        //Boat Size = totalSheepWeight / maxBoatCourses
+        int boatSize = totalSheepWeight / maxBoatCourses;
+        //TODO optimalBoatSize = From Sorted List of sheep weight remove the elements that add up most closely to boat size
+
+        return boatSize;
+    }
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        //Input 1 - 1<= N <= 1000 (Number of sheep); 1<= K <= 1000  (Max Ship Courses);
+        int N = 0, K = 0;
+        //Optimal Sheep Size
+        int optimalSheepSize = 0;
+        // Sum of weight of all sheep
+        int totalSheepWeight = 0;
+
+        // Input 2 - N times Weight of sheep
+        ArrayList <Integer> sheepWeightList = new ArrayList<>();
+
+        //------ User Input ------
+        try {
+            System.out.println(" Please enter: (1) the number of sheep and (2) number of courses:");
+            do {
+                N = userInput.nextInt();
+                K = userInput.nextInt();
+            } while ((N < 1 || N >1000) && (K <1 || K >1000));
+
+        } catch (Exception e)
+        {
+            System.out.println("Wrong Input, please try again!");
+        }
+
+        //Input 2: Weight of sheep
+        System.out.println(" Please enter: the weight of all sheep:");
+        for (int i=0; i<N; i++) {
+            int sheepWeight = userInput.nextInt();
+            if(sheepWeight <1 || sheepWeight >1000) {
+                System.out.println(" Input out of scope, please enter a new value between 1 and 1000");
+                sheepWeight = userInput.nextInt();
+            }
+            else {
+                sheepWeightList.add(sheepWeight);
+                totalSheepWeight+=sheepWeight;
+            }
+        }
+
+        // Output : Max Size of Ship
+        optimalSheepSize = calculateBoatSize(K, totalSheepWeight, sheepWeightList);
+        System.out.println("Ship Size:" + optimalSheepSize);
+
+    }
+}
